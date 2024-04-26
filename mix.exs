bdc801cd73faabd347f7642adce576c57c6cd696
hiyoio <168177874+hiyoio@users.noreply.github.com>
2024-04-26 10:49:48 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule YourApp.MixProject do
+defmodule MyApp.MixProject do
   use Mix.Project
 
   def project do
@@ -6,37 +6,31 @@ defmodule YourApp.MixProject do
       app: :hi-gigalixir,
       version: "0.1.0",
       elixir: "~> 1.13",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
 
-  defp elixirc_paths(:prod), do: ["lib", "priv/repo/migrations"]
-  defp elixirc_paths(_), do: ["lib"]
-
-  defp aliases do
+  def application do
     [
-      setup: ["deps.get", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      extra_applications: [:logger]
     ]
   end
 
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_dashboard, "~> 0.2"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:phoenix, "~> 1.6.6"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.17.5"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.6"},
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.18"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 end
